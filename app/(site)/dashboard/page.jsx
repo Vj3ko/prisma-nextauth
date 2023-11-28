d9ef3ba6b122afb7a89592fa10285acb50805cf2
Vjekoslav Dizdar <vjekoslav.dizdar@globallogic.com>
2023-11-28 12:50:25 +0100

    removed session in dashboard page

--- app/(site)/dashboard/page.jsx
+++ app/(site)/dashboard/page.jsx
@@ -1,16 +1,6 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import User from "@/app/components/user";
-import { getServerSession } from "next-auth";
-// import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
-  const session = await getServerSession(authOptions);
-
-  // if (!session) {
-  //   redirect("/login");
-  // }
-
-  console.log("dashboard", session);
   return (
     <div>
       <h1>Welcome to dashboard</h1>
